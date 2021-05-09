fb983a78c7da79055b2c17268f75c64debfa39bb
emilkozole <emil.kozole@gmail.com>
2021-05-09 15:54:40 +0200

    prvi push

---
+++ questions.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    "question": "Bi raje celo življenje jedel nesoljeno hrano ali alkoholno abstiniral?",
+    "answer1": "Nesoljena hrana",
+    "answer1Total": "1",
+    "answer2": "Alkoholna abstinenca",
+    "answer2Total": "2",
+    "answer3": "Ne vem",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Bi imel svojega klona, ki bi lahko prevzel vso bolečino in neprijetna opravila v zameno za večno življenje v Sloveniji?",
+    "answer1": "Ja",
+    "answer1Total": "1",
+    "answer2": "Ne",
+    "answer2Total": "2",
+    "answer3": "Ne vem",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Bi raje živel z družino v razkošnem penthouseu s pedofili za sosede ali na osameli kmetiji obkoljeni z medvedi in volkovi?",
+    "answer1": "Z družino",
+    "answer1Total": "1",
+    "answer2": "Sam",
+    "answer2Total": "3",
+    "answer3": "Ne vem",
+    "answer3Total": "2"
+  },
+  {
+    "question": "V zameno za branje misli moraš živeti v slow-motion svetu",
+    "answer1": "Ja",
+    "answer1Total": "3",
+    "answer2": "Ne",
+    "answer2Total": "2",
+    "answer3": "Ne vem",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Bi bil raje slep in rasist ali glup in rasist?",
+    "answer1": "Slep in rasist",
+    "answer1Total": "1",
+    "answer2": "Glup in rasist",
+    "answer2Total": "2",
+    "answer3": "Oboje",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Kako se počutiš?",
+    "answer1":
+      "Slabo",
+    "answer1Total": "3",
+    "answer2": "Dobro",
+    "answer2Total": "2",
+    "answer3": "Ne vem",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Za koga navijaš?",
+    "answer1": "NK Maribor",
+    "answer1Total": "1",
+    "answer2": "NK Olimpija",
+    "answer2Total": "2",
+    "answer3": "Nič od tega",
+    "answer3Total": "3"
+  }
+]
