d815c6b5084dd07796daada18bd03454d8778e4d
emilkozole <emil.kozole@gmail.com>
2021-05-31 15:48:09 +0200

    nova verzija

--- questions.js
+++ questions.js
@@ -1,68 +1,103 @@
 const questions = [
   {
-    "question": "Bi raje celo življenje jedel nesoljeno hrano ali alkoholno abstiniral?",
-    "answer1": "Nesoljena hrana",
+    "question": "Stopili ste ob vznožje piramide preporoda. Obhodili jo boste v krogu in opazovali njene štiri ploskve. Vsaka ploskev je povsem drugačna od sosednje in že od daleč je očitno, da ne gre za snovno zgradbo. Med hojo boste razmišljali o piramidi in o nenavadnih stvareh, ki se dogajajo na njenih ploskvah. Ste pripravljeni?",
+    "answer1": "Rojen sem bil za to",
     "answer1Total": "1",
-    "answer2": "Alkoholna abstinenca",
+    "answer2": "Ne razumem",
     "answer2Total": "2",
-    "answer3": "Ne vem",
-    "answer3Total": "3"
+    "answer3": "Nikoli nisem pripravljen na nič",
+    "answer3Total": "3",
+    "answer4": "Strah me je",
+    "answer4Total": "4"
   },
   {
-    "question": "Bi imel svojega klona, ki bi lahko prevzel vso bolečino in neprijetna opravila v zameno za večno življenje v Sloveniji?",
-    "answer1": "Ja",
+    "question": "Začeli ste hoditi. Prva ploskev piramide utripa v živih in mesenih barvah. Na njej so nekako upodobljene entitete iz vaše okolice, le groteskno spačene. Kaj vidite?",
+    "answer1": "Nedoločno množico na poti skozi tunel",
     "answer1Total": "1",
-    "answer2": "Ne",
+    "answer2": "Ljudi z večjo močjo od vaše, ki vas usmerjajo z nevro-krmilom",
     "answer2Total": "2",
-    "answer3": "Ne vem",
-    "answer3Total": "3"
+    "answer3": "Sebe v drugih, nemočne, prezrcaljene",
+    "answer3Total": "3",
+    "answer4": "Dno brez dna, nastalo zaradi norih eksperimentov",
+    "answer4Total": "4"
+  },
+  {
+    "question": "Ploskev vam ponuja, da izberete neko bitje, da bo eno leto ujeto v piramidi in mislilo o svojih napakah, med tem pa bo svojo življenjsko energijo darovalo v dobrodelne namene (podobno kot baterija, ampak nekoliko boleče). Katero bitje kaznujete?",
+    "answer1": "Takšno, kar vam je kdaj ugovarjalo",
+    "answer1Total": "1",
+    "answer2": "Takšno, ki je objektivno slabo v skladu z družbenimi predstavami",
+    "answer2Total": "2",
+    "answer3": "Nekaj nejasnega in dolgočasno nedoločljivega (blob)",
+    "answer3Total": "3",
+    "answer4": "Ničesar",
+    "answer4Total": "4"
   },
   {
     "question":
-      "Bi raje živel z družino v razkošnem penthouseu s pedofili za sosede ali na osameli kmetiji obkoljeni z medvedi in volkovi?",
-    "answer1": "Z družino",
+      "Prispeli ste do prvega kota in se obrnili za 90 stopinj. Naslednja ploskev piramide je bolj senčna in brezoblična, ampak vzbuja manj močna čustva. Srčni utrip vam pojenja. Čeprav je to fizično nemogoče, se zdi, da je ploskev piramide vbočena in lahko hodite skoznjo:",
+    "answer1": "Izogibati se je treba pastem",
     "answer1Total": "1",
-    "answer2": "Sam",
+    "answer2": "Pojdimo proti simbolni maternici",
     "answer2Total": "3",
-    "answer3": "Ne vem",
-    "answer3Total": "2"
+    "answer3": "Ne verjamem optičnim iluzijam",
+    "answer3Total": "2",
+    "answer4": "poskusimo plezati po kapnikih",
+    "answer4Total": "4"
   },
   {
-    "question": "V zameno za branje misli moraš živeti v slow-motion svetu",
-    "answer1": "Ja",
+    "question": "Vbočena ploskev vas je vsrkala ne glede na vašo odločitev. Zdaj plavate v nekakšni prajuhi in ste priča prvim stvarem. Lahko postanete bog specifičnega stvarstva. Kakšen bog ste?",
+    "answer1": "Med in mleko",
     "answer1Total": "3",
-    "answer2": "Ne",
+    "answer2": "Žveplo in toča",
     "answer2Total": "2",
-    "answer3": "Ne vem",
-    "answer3Total": "1"
+    "answer3": "Moji roboti me ne zanimajo tako zelo",
+    "answer3Total": "1",
+    "answer4": "Jeza -> obžalovanje -> Jeza -> obžalovanje",
+    "answer4Total": "4"
   },
   {
-    "question": "Bi bil raje slep in rasist ali glup in rasist?",
-    "answer1": "Slep in rasist",
+    "question": "Spet se obrnete za 90 stopinj in naslednja ploskev vas popolnoma osupne. Pulzirajoča ekstaza, ki emanira s te ploskve, je brez vseh čutnozaznavnih lastnosti razen drhteče basovske intenzivnosti, ki prodira v kosti.",
+    "answer1": "Okrog piramide raste šašje. Zamašite si ušesa z mehko, naravno vatico",
     "answer1Total": "1",
-    "answer2": "Glup in rasist",
+    "answer2": "To je raj, tukaj hočem biti za vedno",
     "answer2Total": "2",
-    "answer3": "Oboje",
-    "answer3Total": "3"
+    "answer3": "Kuriozen fenomen, le kaj ga povzroča?",
+    "answer3Total": "3",
+    "answer4": "Preglasite piramido",
+    "answer4Total": "4"
   },
   {
     "question":
-      "Kako se počutiš?",
-    "answer1":
-      "Slabo",
+      "Kakor sem rekel, vibriranje te ploskve prodira v kosti in se mu je nemogoče izogniti, ne na razumski ne na čutni način. Kako se sprijaznite s tem?",
+    "answer1": "Z očmi",
     "answer1Total": "3",
-    "answer2": "Dobro",
+    "answer2": "Z ovijanjem",
     "answer2Total": "2",
-    "answer3": "Ne vem",
-    "answer3Total": "1"
+    "answer3": "Neoporečno",
+    "answer3Total": "1",
+    "answer4": "Z zobovjem",
+    "answer4Total": "4"
   },
   {
-    "question": "Za koga navijaš?",
-    "answer1": "NK Maribor",
+    "question": "Za vami je dolga pot. V mislih imate počitek, ampak zadnja ploskev piramide je najbolj silovita od vseh. Ugotovite, da vsaka ploskev piramide deloma spominja na prejšnjo, deloma pa na naslednjo. Po tejle očitno nenehno polzi magma in se dogajajo nezaslišane stvari. Kateri šport vas najbolj privlači?",
+    "answer1": "Srfanje po magmi",
     "answer1Total": "1",
-    "answer2": "NK Olimpija",
+    "answer2": "Magma šah",
     "answer2Total": "2",
-    "answer3": "Nič od tega",
-    "answer3Total": "3"
+    "answer3": "Plavanje v magmi (* za doplačilo)",
+    "answer3Total": "3",
+    "answer4": "Magmomet",
+    "answer4Total": "4"
+  },
+  {
+    "question": "Magma je bila očitno le nekakšen represiven ukrep, značilen za dimenzijo, v kateri obstaja Četrta ploskev piramide. Na vratih vašega hotela je z nožem pribit list gverilskih borcev za svobodo, ki nasprotujejo komercializaciji te stranice.",
+    "answer1": "Nadaljujem z zabavo (magma me varuje)",
+    "answer1Total": "1",
+    "answer2": "Pridružim se revoluciji",
+    "answer2Total": "2",
+    "answer3": "Potopim se in prižuborim na plan bitje čiste moči, teserakt, Apolon, Šamaš",
+    "answer3Total": "3",
+    "answer4": "Magma se je v resnici karamela",
+    "answer4Total": "4"
   }
 ]
